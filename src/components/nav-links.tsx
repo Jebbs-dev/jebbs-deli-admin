127e64aef837c57129d5876a26b1d7396e095098
Jebbs-dev <ojebiyifulness@gmail.com>
2024-04-05 16:10:32 +0100

    feat: add link styling for smaller screens

--- src/components/nav-links.tsx
+++ src/components/nav-links.tsx
@@ -20,6 +20,17 @@ const links = [
 ];
 
 const NavLinks = () => {
+  let slider;
+  // const slideLeft = () => {
+  //   slider = document.getElementById("slider");
+  //   slider.scrollLeft = slider.scrollLeft - 1000;
+  // };
+
+  // const slideRight = () => {
+  //   slider = document.getElementById("slider");
+  //   slider.scrollLeft = slider.scrollLeft + 1000;
+  // };
+
   const pathname = usePathname();
 
   return (
@@ -31,20 +42,49 @@ const NavLinks = () => {
             key={link.name}
             href={link.href}
             className={clsx(
-              "flex h-7 items-start px-6 text-sm border-l-4 border-opacity-0 hover:text-[#2463EB] hover:text-opacity-90",
+              "hidden md:flex h-7 items-start px-6 text-sm border-l-4 border-transparent hover:text-[#2463EB] hover:text-opacity-90",
               {
-                "text-[#2463EB] border-l-4 border-opacity-100 border-l-[#2463EB] font-medium":
+                "text-[#2463EB] border-opacity-100 font-medium":
                   pathname === link.href,
               }
             )}
           >
             <div className="flex my-auto">
               <LinkIcon className="mr-3" size={20} />
-              <p className="hidden md:block">{link.name}</p>
+              <p className="block">{link.name}</p>
             </div>
           </Link>
         );
       })}
+{/* <div cl>
+
+</div> */}
+      <div
+        id="slider"
+        className="flex p-3 space-x-3 w-full auto-rows-fr overflow-x-scroll scroll whitespace-nowrap scroll-smooth scrollbar-hide"
+      >
+        {links.map((link) => {
+          const LinkIcon = link.icon;
+          return (
+            <Link
+              key={link.name}
+              href={link.href}
+              className={clsx(
+                "h-40 flex justify-center items-center rounded-md text-muted-foreground font-medium border border-[#2463EB] hover:bg-blue-500 hover:text-white md:hidden",
+                {
+                  "text-white bg-[#2463EB] font-medium":
+                    pathname === link.href,
+                }
+              )}
+            >
+              <div className="flex flex-col w-[90vw] space-y-2 justify-center items-center">
+                <LinkIcon size={40} />
+                <p className="text-2xl">{link.name}</p>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
     </>
   );
 };
