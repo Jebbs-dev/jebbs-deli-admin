e10673cacc9d10a54210f207e20cba150e9277bd
Jebbs-dev <ojebiyifulness@gmail.com>
2024-04-15 23:59:10 +0100

    feat: update dashboard layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,18 +1,27 @@
+import DashboardMenu from "@/components/dashboard/dashboard-menu";
 import SideNav from "@/components/side-nav";
 
 interface DashboardLayoutProps {
   children: React.ReactNode;
 }
- 
-const DashboardLayout: React.FC<DashboardLayoutProps> = ({children}) => {
-  return ( 
+
+const DashboardLayout: React.FC<DashboardLayoutProps> = ({ children }) => {
+  return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden bg-[#f7f8fa]">
-      <div className="w-full flex-none md:w-48">
-        <SideNav/>
+      <div className="flex-none w-full md:w-48">
+        <div className="flex-grow p-6 md:hidden">
+          <DashboardMenu />
+        </div>
+        <SideNav />
       </div>
-      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+      <div className="flex-grow md:w-full md:overflow-y-auto md:p-12">
+      <div className="hidden md:block">
+          <DashboardMenu />
+        </div>
+        {children}
+      </div>
     </div>
   );
-}
- 
+};
+
 export default DashboardLayout;
