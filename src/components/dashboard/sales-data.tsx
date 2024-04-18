c8e58fcc9c24fb120383460e4263336ce47e8812
Jebbs-dev <ojebiyifulness@gmail.com>
2024-04-18 10:08:03 +0100

    feat: add tabs for line and bar chart

--- src/components/dashboard/sales-data.tsx
+++ src/components/dashboard/sales-data.tsx
@@ -6,6 +6,8 @@ import {
   monthlySalesData,
   yearlySalesData,
   selectSalesLabel,
+  selectSalesData,
+  selectTImeLabel,
 } from "./data/sales";
 
 import { Bar, Line } from "react-chartjs-2";
@@ -14,77 +16,99 @@ import { Chart, registerables } from "chart.js";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { BarChart3, LineChart } from "lucide-react";
 import { useState } from "react";
-import { selectSalesData } from "./data/sales";
 
 Chart.register(...registerables);
 
-
 const SalesData = () => {
   const [chartTab, setChartTab] = useState("dailyChart");
+  const [selectLine, setSelectLine] = useState(false);
 
   return (
     <>
-      <div className="w-[500px] relative bg-white shadow-sm p-3 rounded-md">
+      <div className="w-[500px] relative bg-white shadow-md p-3 rounded-md">
         <Tabs className="p-3" defaultValue="dailyChart">
-          <TabsList className="flex w-40 relative left-[20%] h-8 mb-4">
-            <TabsTrigger
-              className="px-2 py-1"
-              value="dailyChart"
-              onClick={() => {
-                selectSalesData(dailySalesData);
-                selectSalesLabel(dailySalesData);
-                setChartTab("dailyChart");
-              }}
-            >
-              1d
-            </TabsTrigger>
-            <TabsTrigger
-              className="px-2 py-1"
-              value="weeklyChart"
-              onClick={() => {
-                selectSalesData(weeklySalesData);
-                selectSalesLabel(weeklySalesData);
-                setChartTab("weeklyChart");
-              }}
-            >
-              1w
-            </TabsTrigger>
-            <TabsTrigger
-              className="px-2 py-1"
-              value="monthlyChart"
-              onClick={() => {
-                selectSalesData(monthlySalesData);
-                selectSalesLabel(monthlySalesData);
-                setChartTab("monthlyChart");
-              }}
-            >
-              1m
-            </TabsTrigger>
-            <TabsTrigger
-              className="px-2 py-1"
-              value="yearlyChart"
-              onClick={() => {
-                selectSalesData(yearlySalesData);
-                selectSalesLabel(yearlySalesData);
-                setChartTab("yearlyChart");
-              }}
-            >
-              1y
-            </TabsTrigger>
-            {/* <TabsTrigger className="px-2 py-1" value="lineChart">
-              <LineChart className="w-4 h-4" />
-            </TabsTrigger>
-            <TabsTrigger className="px-2 py-1" value="barChart">
-              <BarChart3 className="w-4 h-4" />
-            </TabsTrigger> */}
-          </TabsList>
+          <div className="flex flex-row space-x-56">
+            <div>
+              <TabsList className="flex w-32 relative left-[20%] h-8 mb-4">
+                <TabsTrigger
+                  className="px-2 py-1 text-xs font-medium"
+                  value="dailyChart"
+                  onClick={() => {
+                    selectSalesData(dailySalesData);
+                    selectSalesLabel(dailySalesData);
+                    selectTImeLabel("Daily");
+                    setChartTab("dailyChart");
+                  }}
+                >
+                  1d
+                </TabsTrigger>
+                <TabsTrigger
+                  className="px-2 py-1 text-xs font-medium"
+                  value="weeklyChart"
+                  onClick={() => {
+                    selectSalesData(weeklySalesData);
+                    selectSalesLabel(weeklySalesData);
+                    selectTImeLabel("Weekly");
+                    setChartTab("weeklyChart");
+                  }}
+                >
+                  1w
+                </TabsTrigger>
+                <TabsTrigger
+                  className="px-2 py-1 text-xs font-medium"
+                  value="monthlyChart"
+                  onClick={() => {
+                    selectSalesData(monthlySalesData);
+                    selectSalesLabel(monthlySalesData);
+                    selectTImeLabel("Monthly");
+                    setChartTab("monthlyChart");
+                  }}
+                >
+                  1m
+                </TabsTrigger>
+                <TabsTrigger
+                  className="px-2 py-1 text-xs font-medium"
+                  value="yearlyChart"
+                  onClick={() => {
+                    selectSalesData(yearlySalesData);
+                    selectSalesLabel(yearlySalesData);
+                    selectTImeLabel("Yearly");
+                    setChartTab("yearlyChart");
+                  }}
+                >
+                  1y
+                </TabsTrigger>
+              </TabsList>
+            </div>
+            <div>
+              <TabsList className="h-8">
+                <TabsTrigger
+                  className="px-2 py-1"
+                  value={chartTab}
+                  onClick={() => setSelectLine(true)}
+                >
+                  <LineChart className="w-4 h-4" />
+                </TabsTrigger>
+                <TabsTrigger
+                  className="px-2 py-1"
+                  value={chartTab}
+                  onClick={() => setSelectLine(false)}
+                >
+                  <BarChart3 className="w-4 h-4" />
+                </TabsTrigger>
+              </TabsList>
+            </div>
+          </div>
 
-          <TabsContent value={chartTab}>
-            <Line data={data} options={options} />
-          </TabsContent>
-          <TabsContent value="barChart">
-            <Bar data={data} options={options} />
-          </TabsContent>
+          {selectLine ? (
+            <TabsContent value={chartTab}>
+              <Line data={data} options={options} />
+            </TabsContent>
+          ) : (
+            <TabsContent value={chartTab}>
+              <Bar data={data} options={options} />
+            </TabsContent>
+          )}
         </Tabs>
       </div>
     </>
