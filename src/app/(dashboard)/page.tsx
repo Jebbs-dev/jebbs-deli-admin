c3eb3c3256371a9c1d22013ab58dee73e14264e2
Jebbs-dev <ojebiyifulness@gmail.com>
2024-04-22 23:12:25 +0100

    feat: add data-table to dashboard page

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,14 +1,19 @@
-import SalesData from "@/components/dashboard/sales-data";
+import { Order } from "@/components/dashboard/data/orders";
+import { columns } from "@/components/dashboard/components/columns";
+import { data } from "@/components/dashboard/data/sales";
+import SalesData from "@/components/dashboard/sales-chart";
 import SummaryData from "@/components/dashboard/summary-data";
+import { DataTable } from "@/components/ui/data-table";
+import { orders } from "@/components/dashboard/data/orders";
 
-interface DashboardPageProps {}
 
-const DashboardPage: React.FC<DashboardPageProps> = () => {
+const DashboardPage = () => {
   return (
 
       <div className="flex flex-col space-y-7 py-6 w-full">
         <SummaryData />
         <SalesData/>
+        <DataTable columns={columns} data={orders}/>
       </div>
   );
 };
