57f729d0f4de803a47ad61b644de9821cf15f483
Jebbs-dev <ojebiyifulness@gmail.com>
2025-04-25 23:41:40 +0100

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,24 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const convertFileToUrl = (file: File) => URL.createObjectURL(file);
+
+export const getBase64 = (file: any) => {
+  return new Promise<string>((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file); // Read the file as a data URL
+    reader.onload = () => {
+      // Check if reader.result is not null before attempting to access it.
+      if (reader.result && typeof reader.result === "string") {
+        resolve(reader.result.split(",")[1]); // Get the Base64 part
+      } else {
+        reject(new Error("FileReader result is null or not a string")); // Handle null or non-string error case
+      }
+    };
+    reader.onerror = (error) => reject(error);
+  });
+};
