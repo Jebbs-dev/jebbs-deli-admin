469ba05dc5699ea059dc9171679595462bedf5dc
Jebbs-dev <ojebiyifulness@gmail.com>
2025-04-25 23:40:49 +0100

    feat: update sidebar

--- src/components/side-nav.tsx
+++ src/components/side-nav.tsx
@@ -1,40 +1,56 @@
+"use client";
+
 import Link from "next/link";
 import NavLinks from "./nav-links";
 import { Settings } from "lucide-react";
 import { LogOut } from "lucide-react";
+import useAuthStore from "@/state-store/auth";
 
 const SideNav = () => {
-
-  const sizeVariants = ["small", "large"]
+  const { isLoggedIn, logout } = useAuthStore();
 
   return (
-    <div className="flex flex-col md:h-full border bg-white text-muted-foreground ">
-      <Link className="hidden md:flex items-start px-8 py-10" href="/">
-        <p className="text-[#2463EB]">deli</p>
-      </Link>
+    <>
+      <div className="flex flex-col fixed h-[100vh] bg-white dark:bg-background text-muted-foreground overflow-auto w-12 md:w-52 lg:w-64">
+        <Link
+          className="flex items-center justify-center md:justify-start px-2 py-6 md:px-7 md:py-10"
+          href="/"
+        >
+          <p className="hidden md:block text-black dark:text-white text-xl md:text-3xl ">
+            deli<span className="text-primary text-2xl md:text-5xl">.</span>
+          </p>
+          <p className="md:hidden text-black dark:text-white text-3xl ">
+            d<span className="text-primary text-2xl md:text-5xl">.</span>
+          </p>
+        </Link>
 
-      <div className="relative flex mx-auto md:mx-0 md:max-w-none w-full md:grow md:justify-between md:flex-col md:space-x-0 md:space-y-5">
-        <NavLinks />
-        <div className="hidden h-auto w-full grow md:block"></div>
-        <div className="hidden md:flex flex-col pb-10">
-          <Link
-            className="flex items-start px-8 py-7 text-sm border-l-4 border-transparent hover:text-[#2463EB] hover:text-opacity-90"
-            href="/dashboard/settings"
-          >
-            <div className="flex my-auto">
-              <Settings className="mr-3" size={20} />
-              <p className="block">Settings</p>
-            </div>
-          </Link>
-          <button className="flex items-start px-8 text-sm border-l-4 border-transparent">
-          <div className="flex my-auto">
-              <LogOut className="mr-3" size={20} />
-              <p className="hidden md:block">Logout</p>
-            </div>
-          </button>
+        <div className="flex flex-col h-full justify-between">
+          <div className="flex flex-col gap-1 mt-4">
+            <NavLinks />
+          </div>
+          <div className="mt-auto flex flex-col sm:px-0 sm:items-center md:items-start md:px-7 pb-10">
+            <Link
+              className="flex items-center justify-center md:justify-start py-5 text-sm text-primary hover:text-orange-600 hover:text-opacity-90"
+              href="/dashboard/settings"
+            >
+              <div className="flex">
+                <Settings className="sm:mr-0 md:mr-3" size={20} />
+                <p className="hidden md:block">Settings</p>
+              </div>
+            </Link>
+            <button
+              className="flex items-center justify-center md:justify-start py-3 text-sm text-primary hover:text-orange-600 hover:text-opacity-90"
+              onClick={logout}
+            >
+              <div className="flex">
+                <LogOut className="sm:mr-0 md:mr-3" size={20} />
+                <p className="hidden md:block">Logout</p>
+              </div>
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
