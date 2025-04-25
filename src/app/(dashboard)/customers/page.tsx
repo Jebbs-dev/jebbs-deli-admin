7037a2a098025ec6415cc30c6b98915fb7120d82
Jebbs-dev <ojebiyifulness@gmail.com>
2025-04-25 23:38:33 +0100

    update customer page

--- src/app/(dashboard)/customers/page.tsx
+++ src/app/(dashboard)/customers/page.tsx
@@ -1,7 +1,16 @@
+"use client"
+
+import CustomerList from "@/modules/customers/components/customer-list";
+import { useFetchCustomer } from "@/modules/customers/queries/fetch-customers";
+
+
  
 const CustomersPage = () => {
+
   return ( 
-    <p>Customers Page</p>
+    <div className="flex flex-col space-y-7 py-6 w-full" >
+    <CustomerList />
+  </div>
    );
 }
  
