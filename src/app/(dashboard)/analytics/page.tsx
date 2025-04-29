d040b55c1a89064b69a3a93444539d14d1e3c49e
Jebbs-dev <ojebiyifulness@gmail.com>
2025-04-29 20:39:30 +0100

    Update page.tsx

--- src/app/(dashboard)/analytics/page.tsx
+++ src/app/(dashboard)/analytics/page.tsx
@@ -1,8 +1,11 @@
- 
+import { Component } from "@/modules/analytics/components/chart-one";
+
 const AnalyticsPage = () => {
-  return ( 
-    <p>Analytics Page</p>
-   );
-}
- 
+  return (
+    <div>
+      Analytics Page
+    </div>
+  );
+};
+
 export default AnalyticsPage;
