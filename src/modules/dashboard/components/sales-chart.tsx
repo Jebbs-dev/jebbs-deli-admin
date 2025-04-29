fc84d2335c570ae0208f1f90c3be12670a0f5487
Jebbs-dev <ojebiyifulness@gmail.com>
2025-04-29 20:55:14 +0100

    Update sales-chart.tsx

--- src/modules/dashboard/components/sales-chart.tsx
+++ src/modules/dashboard/components/sales-chart.tsx
@@ -1,127 +1,262 @@
 "use client";
 
+import { useState, useMemo } from "react";
 import {
-  dailySalesData,
-  weeklySalesData,
-  monthlySalesData,
-  yearlySalesData,
-  selectSalesLabel,
-  selectSalesData,
-  selectTImeLabel,
-} from "../data/dummy-sales";
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Line,
+  XAxis,
+  YAxis,
+  AreaChart,
+  Area,
+} from "recharts";
 
-import { Bar, Line } from "react-chartjs-2";
-import { data, options } from "../data/dummy-sales";
-import { Chart, registerables } from "chart.js";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { dailySales } from "../data/dummy-sales-data";
+import { weeklySales } from "../data/dummy-sales-data";
+import { monthlySales } from "../data/dummy-sales-data";
+import { yearlySales } from "../data/dummy-sales-data";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { BarChart3, LineChart } from "lucide-react";
-import { useState } from "react";
 
-Chart.register(...registerables);
+const chartConfig = {
+  views: {
+    label: "No of Sales",
+  },
+  daily: {
+    label: "Daily Sales",
+    color: "#ea580c",
+  },
+  weekly: {
+    label: "Weekly Sales",
+    color: "#ea580c",
+  },
+  monthly: {
+    label: "Monthly Sales",
+    color: "#ea580c",
+  },
+  yearly: {
+    label: "Yearly Sales",
+    color: "#ea580c",
+  },
+} satisfies ChartConfig;
 
-const SalesData = () => {
-  const [chartTab, setChartTab] = useState("dailyChart");
-  const [selectLine, setSelectLine] = useState(false);
-  let lineChart;
-  let barChart = "";
+ const SalesData =()=> {
+  const [activeChart, setActiveChart] =
+    useState<keyof typeof chartConfig>("daily");
+
+  const [salesData, setSalesData] = useState(dailySales);
+  const [selectLineChart, setSelectLineChart] = useState(false);
+
+  const total = useMemo(
+    () => ({
+      daily: dailySales.reduce((acc, curr) => acc + curr.value, 0),
+      weekly: weeklySales.reduce((acc, curr) => acc + curr.value, 0),
+      monthly: monthlySales.reduce((acc, curr) => acc + curr.value, 0),
+      yearly: yearlySales.reduce((acc, curr) => acc + curr.value, 0),
+    }),
+    []
+  );
 
   return (
-    <>
-      <div className="w-full md:h-[350px] relative bg-white border p-2 md:p-3 rounded-md">
-        <h1 className="text-xl font-semibold px-3 md:px-10">Sales Data</h1>
-        <Tabs className="p-3" defaultValue="dailyChart">
-          <div className="flex flex-row justify-between">
-            <div>
-              <TabsList className="flex w-[135px] relative md:left-[20%] h-8 mb-4">
-                <TabsTrigger
-                  className="px-2 py-1 text-xs font-medium"
-                  value="dailyChart"
-                  onClick={() => {
-                    selectSalesData(dailySalesData);
-                    selectSalesLabel(dailySalesData);
-                    selectTImeLabel("Daily");
-                    setChartTab("dailyChart");
-                  }}
-                >
-                  1d
-                </TabsTrigger>
-                <TabsTrigger
-                  className="px-2 py-1 text-xs font-medium"
-                  value="weeklyChart"
-                  onClick={() => {
-                    selectSalesData(weeklySalesData);
-                    selectSalesLabel(weeklySalesData);
-                    selectTImeLabel("Weekly");
-                    setChartTab("weeklyChart");
-                  }}
-                >
-                  1w
-                </TabsTrigger>
-                <TabsTrigger
-                  className="px-2 py-1 text-xs font-medium"
-                  value="monthlyChart"
-                  onClick={() => {
-                    selectSalesData(monthlySalesData);
-                    selectSalesLabel(monthlySalesData);
-                    selectTImeLabel("Monthly");
-                    setChartTab("monthlyChart");
-                  }}
-                >
-                  1m
-                </TabsTrigger>
-                <TabsTrigger
-                  className="px-2 py-1 text-xs font-medium"
-                  value="yearlyChart"
-                  onClick={() => {
-                    selectSalesData(yearlySalesData);
-                    selectSalesLabel(yearlySalesData);
-                    selectTImeLabel("Yearly");
-                    setChartTab("yearlyChart");
-                  }}
-                >
-                  1y
-                </TabsTrigger>
-              </TabsList>
-            </div>
-            <div>
-              <TabsList className="h-8">
-                <TabsTrigger
-                  className="px-2 py-1"
-                  value={chartTab}
-                  onClick={() => {
-                    setSelectLine(true)
-                    lineChart = chartTab;
-                  }}
-                >
-                  <LineChart className="w-4 h-4" />
-                </TabsTrigger>
-                <TabsTrigger
-                  className="px-2 py-1"
-                  onClick={() => {
-                    setSelectLine(false)
-                    barChart = chartTab;
-                  }}
-                  value={barChart}
-                >
-                  <BarChart3 className="w-4 h-4" />
-                </TabsTrigger>
-              </TabsList>
-            </div>
+    <Tabs defaultValue="daily">
+      <Card className="">
+        <CardHeader className="grid grid-cols-3 space-y-0 p-0">
+          <div className="flex flex-1 flex-col justify-center gap-1 px-6 py-5 sm:py-6">
+            <CardTitle className="text-primary">Sales Data</CardTitle>
+            <CardDescription>Showing total sales</CardDescription>
           </div>
-
-          {selectLine ? (
-            <TabsContent value={chartTab}>
-              <Line data={data} options={options} />
-            </TabsContent>
+          <div className="py-5 px-6">
+            <TabsList className="flex w-[125px] relative h-8 mb-4">
+              <TabsTrigger
+                className="px-2 py-1 text-xs font-medium"
+                value="daily"
+                onClick={() => {
+                  setActiveChart("daily");
+                  setSalesData(dailySales);
+                }}
+              >
+                1d
+              </TabsTrigger>
+              <TabsTrigger
+                className="px-2 py-1 text-xs font-medium"
+                value="weekly"
+                onClick={() => {
+                  setActiveChart("weekly");
+                  setSalesData(weeklySales);
+                }}
+              >
+                1w
+              </TabsTrigger>
+              <TabsTrigger
+                className="px-2 py-1 text-xs font-medium"
+                value="monthly"
+                onClick={() => {
+                  setActiveChart("monthly");
+                  setSalesData(monthlySales);
+                }}
+              >
+                1m
+              </TabsTrigger>
+              <TabsTrigger
+                className="px-2 py-1 text-xs font-medium"
+                value="yearly"
+                onClick={() => {
+                  setActiveChart("daily");
+                  setSalesData(yearlySales);
+                }}
+              >
+                1y
+              </TabsTrigger>
+            </TabsList>
+          </div>
+          <div className="py-5 px-6">
+            <TabsList className="h-8">
+              <TabsTrigger
+                className="px-2 py-1"
+                onClick={() => {
+                  setSelectLineChart(false);
+                }}
+                value="bar"
+              >
+                <BarChart3 className="w-4 h-4" />
+              </TabsTrigger>
+              <TabsTrigger
+                className="px-2 py-1"
+                onClick={() => {
+                  setSelectLineChart(true);
+                }}
+                value="line"
+              >
+                <LineChart className="w-4 h-4" />
+              </TabsTrigger>
+            </TabsList>
+          </div>
+        </CardHeader>
+        <CardContent className="px-2 sm:p-6">
+          {selectLineChart ? (
+            <ChartContainer
+              config={chartConfig}
+              className="aspect-auto h-[250px] w-full"
+            >
+              <AreaChart
+                accessibilityLayer
+                data={salesData}
+                margin={{
+                  left: 12,
+                  right: 12,
+                }}
+              >
+                <CartesianGrid vertical={false} />
+                <YAxis
+                  dataKey="value"
+                  tickLine={false}
+                  axisLine={false}
+                  label={{ value: "Sales", angle: -90, position: "insideLeft" }}
+                />
+                <XAxis
+                  dataKey="saleTime"
+                  tickLine={false}
+                  axisLine={false}
+                  tickMargin={8}
+                  tickFormatter={(value) => value}
+                />
+                <ChartTooltip
+                  content={
+                    <ChartTooltipContent
+                      className="w-[150px]"
+                      nameKey="views"
+                      labelFormatter={(value) => {
+                        return value.toString();
+                      }}
+                      indicator="line"
+                    />
+                  }
+                />
+                <Area
+                  dataKey="value"
+                  type="natural"
+                  stroke={`var(--color-${activeChart})`}
+                  fill={`var(--color-${activeChart})`}
+                  fillOpacity={0.1}
+                  strokeWidth={1.5}
+                  dot={false}
+                />
+              </AreaChart>
+            </ChartContainer>
           ) : (
-            <TabsContent value={chartTab}>
-              <Bar data={data} options={options} />
-            </TabsContent>
+            <ChartContainer
+              config={chartConfig}
+              className="aspect-auto h-[250px] w-full"
+            >
+              <BarChart
+                accessibilityLayer
+                data={salesData}
+                margin={{
+                  left: 12,
+                  right: 12,
+                  bottom: 12,
+                }}
+              >
+                <CartesianGrid vertical={false} horizontal={false} />
+                <YAxis
+                  dataKey="value"
+                  tickLine={false}
+                  axisLine={false}
+                  label={{ value: "Sales", angle: -90, position: "insideLeft" }}
+                />
+                <XAxis
+                  dataKey="saleTime"
+                  label={{
+                    value: "Time",
+                    offset: -9,
+                    position: "insideBottom",
+                  }}
+                  tickLine={false}
+                  axisLine={true}
+                  tickMargin={8}
+                  minTickGap={32}
+                  tickFormatter={(value) => value}
+                />
+                <ChartTooltip
+                  content={
+                    <ChartTooltipContent
+                      className="w-[150px]"
+                      nameKey="views"
+                      labelFormatter={(value) => {
+                        return value.toString();
+                      }}
+                    />
+                  }
+                />
+                <Bar
+                  dataKey="value"
+                  radius={2}
+                  fill={`var(--color-${activeChart})`}
+                />
+              </BarChart>
+            </ChartContainer>
           )}
-        </Tabs>
-      </div>
-    </>
+        </CardContent>
+      </Card>
+    </Tabs>
   );
-};
+}
 
 export default SalesData;
