d8179b5cd8754c9abdc3c1bee93461bb24a97d57
Jebbs-dev <ojebiyifulness@gmail.com>
2025-04-29 20:50:33 +0100

    feat: update customer list

--- src/modules/customers/components/customer-list.tsx
+++ src/modules/customers/components/customer-list.tsx
@@ -1,31 +1,54 @@
 "use client";
 
-import { DataTable } from "@/components/ui/data-table";
 import React from "react";
 // import { customers } from '../data/customers'
 import { columns as CustomerListColumns } from "./columns/customer-list-column";
 import { useFetchCustomer } from "../queries/fetch-customers";
 import { Customer } from "@/types/user";
-
-
+import { useFetchFilteredCustomers } from "../queries/fetch-filtered-customers";
+import useAuthStore from "@/state-store/auth";
+import { useQueryParamaters } from "@/state-store/use-query-params";
+import { useDebounce } from "@/hooks/use-debounce";
+import { ProductListSkeleton } from "@/modules/products/components/product-list";
+import { DataTable } from "./columns/customer-data-table";
+import TableFilters from "@/components/date-filters";
 
 const CustomerList = () => {
-  const { data: customers, isLoading} = useFetchCustomer();
+  const { vendor } = useAuthStore();
+  const {
+    querykey,
+    setQueryKey,
+    backendPagination,
+    dateFilter,
+    setDateFilter,
+  } = useQueryParamaters();
+
+  const debouncedQuery = useDebounce(querykey, 1000);
+
+  const { data: customers, isLoading } = useFetchFilteredCustomers({
+    ...backendPagination,
+    search: debouncedQuery,
+    startDate: dateFilter.startDate,
+    endDate: dateFilter.endDate,
+  });
 
   if (isLoading) {
-    return <div>Loading...</div>;
+    return <ProductListSkeleton />;
   }
 
-  console.log(customers);
-
   return (
-    <div>
+    <div className="mx-3 px-3 md:mx-0 md:px-10 py-4 border rounded-md bg-white dark:bg-background">
+      <TableFilters title="Customers" canAddNew={false} />
+
       <DataTable
         columns={CustomerListColumns}
-        data={customers || []}
+        data={customers?.customers}
+        total={customers?.total}
+        limit={customers?.limit}
+        offset={customers?.offset}
         showInput
         showAdvancedPagination
-        placeholder="Search for customers..."
+        placeholder="Search for orders..."
         columnFilterKey="name"
       />
     </div>
