dfc5a8b5478ad2c789d0077f30774ac7e4708f4d
Jebbs-dev <ojebiyifulness@gmail.com>
2025-04-29 20:40:36 +0100

    feat: add product store page

---
+++ src/app/(dashboard)/product-store/page.tsx
@@ -0,0 +1,8 @@
+import ProductStoreList from "@/modules/product-store/components/product-store-list";
+import React from "react";
+
+const ProductStorePage = () => {
+  return <ProductStoreList />;
+};
+
+export default ProductStorePage;
