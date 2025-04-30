5290d698f461a313621d5d8a5c0de48b1adf0e69
Jebbs-dev <ojebiyifulness@gmail.com>
2025-04-30 09:17:08 +0100

    feat: update product form

--- src/modules/products/new/components/product-form.tsx
+++ src/modules/products/new/components/product-form.tsx
@@ -3,6 +3,8 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import { useEffect, useMemo, useState } from "react";
+import { convertFileToUrl } from "@/lib/utils";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -22,42 +24,142 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { categories } from "@/modules/products/data/product-list-data";
+import { categories } from "@/modules/products/data/categories";
 import { Checkbox } from "@/components/ui/checkbox";
+import FileUploader from "@/components/file-uploader";
+import { size } from "../../data/size";
+import { Product } from "@/types/products";
+import { useUpdateProduct } from "../../mutations/update-product";
+import { getChangedValues } from "@/utils/get-changed-values";
+import { FaSpinner } from "react-icons/fa";
+import { useToast } from "@/hooks/use-toast";
 
 const formSchema = z.object({
-  image: z.string().min(2),
-  name: z.string().min(2),
-  price: z.number().min(2),
-  status: z.string().min(2),
+  name: z.string().min(2, "Name must not be less than 2 characters"),
+  description: z.string().optional(),
+  price: z.coerce
+    .number()
+    .min(1, "Price must not be less than or equal to zero"),
+  image: z.custom<File[]>().optional(),
+  stock: z.coerce.number(),
   category: z.string().min(2),
-  unitsSold: z.number().min(2),
-  views: z.number().min(2),
-  earnings: z.number().min(2),
+  size: z.any().optional(),
+  storeTag: z.string().optional(),
   isAvailable: z.boolean().default(false).optional(),
-  isUnavailable: z.boolean().default(false).optional(),
+  isFeatured: z.boolean().default(false).optional(),
 });
 
 export type ProductFormValues = z.infer<typeof formSchema>;
 
-const ProductForm = () => {
+interface ProductProps {
+  product: Partial<Product>;
+  productId: string;
+}
+
+const ProductForm = ({ product, productId }: ProductProps) => {
+  const { mutateAsync: updateProduct, isPending } = useUpdateProduct(productId);
+  const { toast } = useToast();
+
+  const [imageFiles, setImageFiles] = useState<File[]>([]);
+
+  const defaultFormValues = {
+    name: product?.name || "",
+    description: product?.description || "",
+    image: imageFiles,
+    price: product?.price || 0,
+    stock: product?.stock || 0,
+    category: product?.category || "",
+    size: product?.size || "",
+    storeTag: product?.storeTag || "",
+    isAvailable: product?.isAvailable || false,
+    isFeatured: product?.isFeatured || false,
+  };
+
   const form = useForm<ProductFormValues>({
     resolver: zodResolver(formSchema),
-    defaultValues: {
-      name: "",
-      image: "",
-      price: 0,
-      unitsSold: 0,
-      category: "",
-      views: 0,
-      earnings: 0,
-      isAvailable: false,
-      isUnavailable: false,
-    },
+    defaultValues: defaultFormValues,
   });
 
+  useEffect(() => {
+    const fetchImage = async () => {
+      if (product?.image && typeof product?.image === "string") {
+        const response = await fetch(product?.image);
+        const blob = await response.blob();
+        const file = new File([blob], "uploaded_image.jpg", {
+          type: blob.type,
+        });
+        setImageFiles([file]);
+      }
+    };
+
+    fetchImage();
+  }, [product?.image]);
+
+  useEffect(() => {
+    form.reset(defaultFormValues);
+  }, [form]);
+
+  const initialValues = useMemo(() => defaultFormValues, []);
+
   const onSubmit = async (values: ProductFormValues) => {
-    console.log(values);
+    const imageValue = values.image && values.image[0];
+
+    const formData = new FormData();
+    const currentValues = form.getValues();
+    const changedValues = getChangedValues<ProductFormValues>(
+      initialValues,
+      currentValues
+    );
+
+    Object.entries(changedValues).forEach(([key, value]) => {
+      if (value !== undefined) {
+        if (Array.isArray(value)) {
+          // Handle arrays (tags)
+          formData.append(key, JSON.stringify(value));
+        } else if (value instanceof File) {
+          // Handle File objects
+          value = imageValue;
+          formData.append(key, value);
+        } else {
+          // Handle strings and booleans
+          formData.append(key, value.toString());
+        }
+      }
+    });
+
+    if (imageValue && imageValue instanceof File) {
+      formData.append("image", imageValue);
+    }
+
+    // formData.append("name", values.name);
+    // formData.append("description", values.description || "");
+    // formData.append("price", values.price.toString());
+    // formData.append("stock", values.stock.toString());
+    // formData.append("category", values.category);
+    // formData.append("size", values.size || "");
+    // formData.append("isAvailable", values.isAvailable ? "true" : "false");
+    // formData.append("isFeatured", values.isFeatured ? "true" : "false");
+
+    // console.log(imageValue);
+
+    // const formProps = Object.fromEntries(formData);
+    // console.log("FormProps", formProps);
+    try {
+      await updateProduct(formData);
+      toast({
+        title: "Success",
+        description: "Updated product successfully!",
+      });
+    } catch (error: any) {
+      console.error("Error updating product:", error);
+      toast({
+        variant: "destructive",
+        title: "Error",
+        description: error.message,
+      });
+    }
+
+    // await updateProduct(values);
   };
 
   return (
@@ -70,20 +172,7 @@ const ProductForm = () => {
             <FormItem>
               <FormLabel>Name</FormLabel>
               <FormControl>
-                <Input placeholder="Product Name" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="image"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Image</FormLabel>
-              <FormControl>
-                <Input placeholder="Upload image" {...field} />
+                <Input placeholder="Product name" {...field} />
               </FormControl>
               <FormMessage />
             </FormItem>
@@ -92,12 +181,32 @@ const ProductForm = () => {
 
         <FormField
           control={form.control}
-          name="name"
+          name="description"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Name</FormLabel>
+              <FormLabel>Description</FormLabel>
               <FormControl>
-                <Input placeholder="Product Name" {...field} />
+                <Input placeholder="Product description" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="image"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Image</FormLabel>
+              <FormControl>
+                <FileUploader
+                  files={imageFiles}
+                  onChange={(files) => {
+                    setImageFiles(files);
+                    field.onChange(files);
+                  }}
+                />
               </FormControl>
               <FormMessage />
             </FormItem>
@@ -111,27 +220,38 @@ const ProductForm = () => {
             <FormItem>
               <FormLabel>Price</FormLabel>
               <FormControl>
-                <Input placeholder="9.99" {...field} />
+                <Input placeholder="9.99" {...field} type="number" />
               </FormControl>
               <FormMessage />
             </FormItem>
           )}
         />
+
+        <FormField
+          control={form.control}
+          name="stock"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Stock</FormLabel>
+              <FormControl>
+                <Input placeholder="0" {...field} type="number" />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
         <FormField
           control={form.control}
           name="category"
           render={({ field }) => (
             <FormItem>
               <FormLabel>Category</FormLabel>
-              <Select
-                onValueChange={field.onChange}
-                value={field.value}
-                defaultValue={field.value}
-              >
+              <Select onValueChange={field.onChange}>
                 <FormControl>
                   <SelectTrigger>
                     <SelectValue
-                      defaultValue={field.value}
+                      // value={field.value}
                       placeholder="Select a category"
                     />
                   </SelectTrigger>
@@ -148,6 +268,46 @@ const ProductForm = () => {
             </FormItem>
           )}
         />
+
+        <FormField
+          control={form.control}
+          name="size"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Size</FormLabel>
+              <Select onValueChange={field.onChange}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Select a size" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  {size.map((size) => (
+                    <SelectItem key={size.value} value={size.value}>
+                      {size.label}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="storeTag"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Store Tag</FormLabel>
+              <FormControl>
+                <Input placeholder="Store tag" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
         <FormField
           control={form.control}
           name="isAvailable"
@@ -162,7 +322,8 @@ const ProductForm = () => {
               <div className="space-y-1 leading-none">
                 <FormLabel>Available</FormLabel>
                 <FormDescription>
-                  This product will appear on the homepage.
+                  This product will appear in the shop. If unchecked, the
+                  product will be shown as out-of-stock in the shop.
                 </FormDescription>
               </div>
             </FormItem>
@@ -170,7 +331,7 @@ const ProductForm = () => {
         />
         <FormField
           control={form.control}
-          name="isUnavailable"
+          name="isFeatured"
           render={({ field }) => (
             <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4">
               <FormControl>
@@ -180,15 +341,27 @@ const ProductForm = () => {
                 />
               </FormControl>
               <div className="space-y-1 leading-none">
-                <FormLabel>Out of Stock</FormLabel>
+                <FormLabel>Is Featured</FormLabel>
                 <FormDescription>
-                  This product will not appear on the homepage.
+                  This product will appear in the shop. If unchecked, the
+                  product will not be displayed in the shop.
                 </FormDescription>
               </div>
             </FormItem>
           )}
         />
-        <Button type="submit">Submit</Button>
+        <Button type="submit">
+          {isPending ? (
+            <>
+              Submitting
+              <span className="ml-2 text-sm">
+                <FaSpinner className="animate-spin" />
+              </span>
+            </>
+          ) : (
+            <>Submit </>
+          )}
+        </Button>
       </form>
     </Form>
   );
