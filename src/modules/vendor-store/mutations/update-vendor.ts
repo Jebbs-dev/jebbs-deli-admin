8d5c3445bf337097c5aa033c0a7df1c869db925e
Jebbs-dev <ojebiyifulness@gmail.com>
2025-04-30 09:20:20 +0100

    feat: add mutation to update vendor admin

---
+++ src/modules/vendor-store/mutations/update-vendor.ts
@@ -0,0 +1,21 @@
+import { Vendor } from "@/types/user";
+import api from "@/utils/api";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+export const useUpdateVendor = (vendorId: string) => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async (data: Partial<Vendor>) => {
+      const response = await api.patch(`/users/${vendorId}`, data);
+      return response.data;
+    },
+    onSuccess: () => {
+      // Refetch the vendor data after the update
+      queryClient.invalidateQueries({ queryKey: ["vendor"] });
+    },
+    onError: (error) => {
+      console.error("Error updating vendor:", error);
+    },
+  });
+};
