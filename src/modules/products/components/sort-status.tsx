050df74f2743203427fa532ab8bb4fac178afae9
Jebbs-dev <ojebiyifulness@gmail.com>
2025-04-30 09:15:47 +0100

    update sort status for available products

--- src/modules/products/components/sort-status.tsx
+++ src/modules/products/components/sort-status.tsx
@@ -1,4 +1,4 @@
-import { Product } from "../data/product-list-data";
+import { Product } from "@/types/products";
 
 interface SortProductStatus {
   data: Product;
@@ -11,25 +11,16 @@ const SortProductStatus: React.FC<SortProductStatus> = ({ data }) => {
         className={`
       px-3 py-1 flex flex-row shrink-0 space-x-3 items-center rounded-sm
       ${
-        data.status === "Out of stock"
+        data.isAvailable
+          ? "bg-green-200 text-green-700 text-opacity-90"
+          : data.stock === 0
           ? "bg-red-200 text-red-700 text-opacity-90"
           : ""
       }
-      ${
-        data.status === "Available"
-          ? "bg-green-200 text-green-700 text-opacity-90"
-          : ""
-      }
+      
     `}
       >
-        <span
-          className={`
-        rounded-full shrink-0 h-2 w-2
-      ${data.status === "Out of stock" ? "bg-red-700" : ""}
-      ${data.status === "Available" ? "bg-green-700" : ""}
-    `}
-        ></span>
-        <p>{data.status}</p>
+        <p>{data.isAvailable ? "Available" : "Out of Stock"}</p>
       </div>
     </div>
   );
