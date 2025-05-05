6978c3835ae7295b294511469567a4f402bd8844
Jebbs-dev <ojebiyifulness@gmail.com>
2025-05-05 18:37:40 +0100

    update dashboard page

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,23 +1,23 @@
-"use client"
+"use client";
 
 import RecentOrders from "@/modules/orders/components/recent-orders";
 import SalesData from "@/modules/dashboard/components/sales-chart";
-import SummaryData from "@/modules/dashboard/components/summary-data";
+import SummaryData from "@/modules/dashboard/components/data-summary";
 
 import TopProducts from "@/modules/dashboard/components/top-products";
 import useAuthStore from "@/state-store/auth";
 
 const DashboardPage = () => {
- 
   return (
     <div className="flex flex-col space-y-7 py-6 w-full">
       <SummaryData />
-      <div className="grid grid-cols-1 gap-2 mx-3 md:mx-0 md:grid-cols-2 md:gap-4">
+      <div className="mx-3 md:mx-0">
         <SalesData />
-        <TopProducts/>
       </div>
-      <RecentOrders/>
-      
+      <div className="grid grid-cols-1 gap-2 mx-3 md:mx-0 md:grid-cols-2 md:gap-4">
+        {/* <TopProducts/> */}
+      </div>
+      <RecentOrders />
     </div>
   );
 };
