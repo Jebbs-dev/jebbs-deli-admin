7f345596653cc42daf2de95dc8e3721160af0506
Jebbs-dev <ojebiyifulness@gmail.com>
2025-05-05 20:09:29 +0100

    Update page.tsx

--- src/app/(dashboard)/customers/page.tsx
+++ src/app/(dashboard)/customers/page.tsx
@@ -1,17 +1,13 @@
-"use client"
+"use client";
 
 import CustomerList from "@/modules/customers/components/customer-list";
-import { useFetchCustomer } from "@/modules/customers/queries/fetch-customer-count";
 
-
- 
 const CustomersPage = () => {
+  return (
+    <div className="flex flex-col space-y-7 py-6 w-full">
+      <CustomerList />
+    </div>
+  );
+};
 
-  return ( 
-    <div className="flex flex-col space-y-7 py-6 w-full" >
-    <CustomerList />
-  </div>
-   );
-}
- 
 export default CustomersPage;
